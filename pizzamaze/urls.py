33815c83bbdf8641809a49cc65a4ba6e1d4d1b93
Aria-vero-s <ariane.saulnier@gmail.com>
2023-01-30 13:34:51 +0000

    Deployment attempt to Heroku

--- pizzamaze/urls.py
+++ pizzamaze/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("booking.urls")), #Added
-    path('users/', include('django.contrib.auth.urls')),
+    # path('users/', include('django.contrib.auth.urls')),
 ]
